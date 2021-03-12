c148d187e27b87692fc9fe99147eaf7eb919fa28
Cristen Jones <cjones3@mbta.com>
2021-03-12 15:38:20 -0500

    tests: Use --trace option when testing in CI

--- semaphore/mix_test.sh
+++ semaphore/mix_test.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
-mix coveralls.json -u --exclude=wallaby
+mix coveralls.json -u --exclude=wallaby --trace
 bash <(curl -s https://codecov.io/bash) -t "$CODECOV_UPLOAD_TOKEN"
