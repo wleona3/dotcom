ed757a787e58fe8c5955bd12f388750fde86cb24
Cristen Jones <cjones3@mbta.com>
2022-09-09 15:16:17 -0400

    fix: have toggle direction button update redux state
    This currently uses its own separate (non-redux) reducer, which handles the state of the selected direction and route pattern. We leave that mostly alone.

--- apps/site/assets/ts/schedule/components/direction/ScheduleDirectionButton.tsx
+++ apps/site/assets/ts/schedule/components/direction/ScheduleDirectionButton.tsx
@@ -1,7 +1,9 @@
 import React, { ReactElement, Dispatch } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import renderSVG from "../../../helpers/render-svg";
 import icon from "../../../../static/images/icon-change-direction.svg";
 import { MenuAction, toggleDirectionAction } from "./reducer";
+import { getPageDirection, resetDirection } from "../../store/schedule-store";
 
 interface Props {
   dispatch: Dispatch<MenuAction>;
@@ -9,16 +11,25 @@ interface Props {
 
 const ScheduleDirectionButton = ({
   dispatch
-}: Props): ReactElement<HTMLElement> => (
-  <button
-    type="button"
-    className="m-schedule-direction__button btn btn-primary"
-    onClick={() => {
-      dispatch(toggleDirectionAction());
-    }}
-  >
-    {renderSVG("m-schedule-direction__icon", icon)}Change Direction
-  </button>
-);
+}: Props): ReactElement<HTMLElement> => {
+  const selectedDirection = useSelector(getPageDirection);
+  const scheduleDispatch = useDispatch();
 
+  // Reconcile with Redux store managing direction state elsewhere
+  const changeDirection = (): void => {
+    const nextDirection = selectedDirection === 1 ? 0 : 1;
+    scheduleDispatch(resetDirection(nextDirection));
+    dispatch(toggleDirectionAction());
+  };
+
+  return (
+    <button
+      type="button"
+      className="m-schedule-direction__button btn btn-primary"
+      onClick={changeDirection}
+    >
+      {renderSVG("m-schedule-direction__icon", icon)}Change Direction
+    </button>
+  );
+};
 export default ScheduleDirectionButton;
